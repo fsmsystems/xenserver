f34f4fc6921c6b97e810c8c22537a291d9b821f2
fsmsystems <fsm.systems@gmail.com>
2014-05-27 13:44:20 +0200

    version con colores

--- snaps.sh
+++ snaps.sh
@@ -2,10 +2,10 @@
 
 SNAP_LIST=$(xe snapshot-list --minimal| sed "s/\\,/\\ /g")
 for SNAP in  $SNAP_LIST; do
-	DESC=$(xe snapshot-list uuid=$SNAP params=name-label --minimal)
-	echo snapshot = $SNAP - $DESC
-	SNAP_OF=$(xe snapshot-list uuid=$SNAP params=snapshot-of --minimal)
-	NAME_LABEL=$(xe vm-list uuid=$SNAP_OF params=name-label --minimal)
-	echo -en ' + [ \E[0;32m'"\033[1msnapshot of = $SNAP_OF \033[0m] - VM.name-label = $NAME_LABEL\n"
-	
+        DESC=$(xe snapshot-list uuid=$SNAP params=name-label --minimal)
+        SNAP_OF_DATE=$(xe snapshot-list uuid=$SNAP params=snapshot-time --minimal)
+        echo -en "snapshot = $SNAP -\e[93m $DESC\033[0m -\e[36m $SNAP_OF_DATE \033[0m\n"
+        SNAP_OF=$(xe snapshot-list uuid=$SNAP params=snapshot-of --minimal)
+        NAME_LABEL=$(xe vm-list uuid=$SNAP_OF params=name-label --minimal)
+        echo -en ' + [ \E[0;32m'"\033[1msnapshot of = $SNAP_OF \033[0m] - VM.name-label = $NAME_LABEL\n\n"
 done
